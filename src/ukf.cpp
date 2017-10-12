8e52157f10593abeba0c4388d40964cc61c63c13
Takahiro Baba <babatakahiro@gmail.com>
2017-10-12 13:11:56 +0900

    プロセスノイズの適合

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -30,10 +30,10 @@ UKF::UKF() {
   time_us_ = 0;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 10;
+  std_a_ = 0.15;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 5;
+  std_yawdd_ = 0.2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
