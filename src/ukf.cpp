8c2a1748bedb05d8e32933d755759bab2423c511
babatakahiro1983 <32314308+babatakahiro1983@users.noreply.github.com>
2017-10-15 17:31:11 +0900

    Update ukf.cpp

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -30,10 +30,10 @@ UKF::UKF() {
   time_us_ = 0;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.220;
+  std_a_ = 0.230;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.272;
+  std_yawdd_ = 0.282;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
